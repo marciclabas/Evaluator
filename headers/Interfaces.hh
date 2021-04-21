237715b77bdbdd0ec6b58f174720fdd43888e1d8
marciclabas <marci.clabas@gmail.com>
2021-04-21 08:46:42 +0100

    started correcting interface overriding

--- headers/Interfaces.hh
+++ headers/Interfaces.hh
@@ -18,14 +18,13 @@
 */
 class IPrintable {
 public:
-	IPrintable() = delete;
 
 	/**
 	@brief Print the @c IPrintable object to the stdout
 	@pre true
 	@post The @c IPrintable object is printed to the stdout
 	*/
-	void print() const;
+	virtual void print() const = 0;
 
 	/**
 	@brief Print the @c IPrintable object to an output stream
@@ -41,14 +40,13 @@ public:
 */
 class IReadable {
 public:
-	IReadable() = delete;
 	
 	/**
 	@brief Read to the @c IReadable object from the stdin
 	@pre true
 	@post The @c IReadable object is read from the stdin
 	*/
-	void read();
+	virtual void read() = 0;
 
 	/**
 	@brief Read to the @c IReadable object from an input stream
@@ -65,35 +63,34 @@ public:
 template <typename ID, typename T>
 class IContainer {
 public:
-	IContainer() = delete;
 
 	/**
 	@brief Return wheter the @c IContainer object has an element with the given id
 	@pre true
 	@post @c true is returned if there is an element with the given id within the @c IContainer object. If there is not, @c false is returned
 	*/
-	bool containsElement(ID id) const;
+	virtual bool containsElement(ID id) const = 0;
 
 	/**
 	@brief Returns the element with the given ID contained in the @c IContainer object
 	@pre An element with the given id does exist within the @c IContainer object
 	@post An element with the given ID contained in the @c IContainer object is returned
 	*/
-	T& operator[](ID id);
+	virtual T& operator[](ID id) = 0;
 
 	/**
 	@brief Return the number of elements contained in the @c IContainer object
 	@pre true
 	@post The number of elements contained in the @c IContainer object is returned
 	*/
-	int getCount() const;
+	virtual int getCount() const = 0;
 
 	/**
 	@brief Add a new element, given such element's id
 	@pre There is not any element with the given id within the @c IContainer object
 	@post A new element with @c newElementID is added to the @c IContainer object
 	*/
-	void addElement(ID newElementID);
+	virtual void addElement(ID newElementID) = 0;
 };
 
 #endif
--- headers/Problem.hh
+++ headers/Problem.hh
@@ -22,7 +22,69 @@ public:
 	Problem();
 	~Problem();
 
-	// other functionality
+	/* ========================================================IPrintable overriden methods========================================================*/
+
+	/**
+	@brief Print the @c IPrintable object to the stdout
+	@pre true
+	@post The @c IPrintable object is printed to the stdout
+	*/
+	void print() const override;
+
+	/**
+	@brief Print the @c IPrintable object to an output stream
+	@pre true
+	@post The @c IPrintable object is printed to the given output stream
+	*/
+	friend std::ostream& operator<< (std::ostream &out, const IPrintable &printable);
+
+	/* ========================================================IReadable overriden methods========================================================*/
+
+	/**
+	@brief Read to the @c IReadable object from the stdin
+	@pre true
+	@post The @c IReadable object is read from the stdin
+	*/
+	void read() override;
+
+	/**
+	@brief Read to the @c IReadable object from an input stream
+	@pre true
+	@post The @c IReadable object is read from the given input stream
+	*/
+	friend std::istream& operator>> (std::istream &in, IReadable &readable);
+
+	/* ========================================================IContainer overriden methods========================================================*/
+
+	/**
+	@brief Return wheter the @c IContainer object has an element with the given id
+	@pre true
+	@post @c true is returned if there is an element with the given id within the @c IContainer object. If there is not, @c false is returned
+	*/
+	bool containsElement(ID id) const override;
+
+	/**
+	@brief Returns the element with the given ID contained in the @c IContainer object
+	@pre An element with the given id does exist within the @c IContainer object
+	@post An element with the given ID contained in the @c IContainer object is returned
+	*/
+	T& operator[](ID id) override;
+
+	/**
+	@brief Return the number of elements contained in the @c IContainer object
+	@pre true
+	@post The number of elements contained in the @c IContainer object is returned
+	*/
+	int getCount() const override;
+
+	/**
+	@brief Add a new element, given such element's id
+	@pre There is not any element with the given id within the @c IContainer object
+	@post A new element with @c newElementID is added to the @c IContainer object
+	*/
+	void addElement(ID newElementID) override;
+
+	/* ===========================================================other functionality===========================================================*/
 
 	/**
 	@brief Updates the <tt>Problem</tt>'s stats based on the submission result
