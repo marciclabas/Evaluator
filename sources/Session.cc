2e3ca28283bf06a79a9435076918ec39b2cde88d
marciclabas <marci.clabas@gmail.com>
2021-04-29 12:27:51 +0200

    fixed n tested bintree<prb::ID> io

--- sources/Session.cc
+++ sources/Session.cc
@@ -7,8 +7,12 @@ Session::~Session() {}
 
 /*==============================================================overrided IO methods============================================================*/
 
+// postorder: tested!
 std::ostream& operator<< (std::ostream & out, const BinTree<prb::ID> & tree) {
-	out << tree.left() << tree.right() << tree.value();
+	if(tree.empty())
+		out << "0 " ;
+	else
+		out << tree.left() << tree.right() << tree.value() << ' ';
 	return out;
 }
 
@@ -16,13 +20,14 @@ void Session::print() const {
 	std::cout << count << ' ' << problems;
 }
 
+// preorder: tested!
 std::istream& operator>> (std::istream & in, BinTree<prb::ID> & tree) {
 	prb::ID problemID; in >> problemID;
 	BinTree<prb::ID> leftChild, rightChild;
 	in >> leftChild >> rightChild;
 	tree = BinTree<prb::ID>(problemID, leftChild, rightChild);
 	return in;
-}
+}}
 
 void Session::read() {
 	std::cin >> problems;
